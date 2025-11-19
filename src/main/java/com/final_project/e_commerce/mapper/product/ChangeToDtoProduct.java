873d643ce0220083d3742450b849872b816978a3
jimmybb7 <jimmychukfun@gmail.com>
2025-11-19 16:51:20 +0800

    change ChangeToDtoProduct interface code but still not success to add MapStruct function.

--- src/main/java/com/final_project/e_commerce/mapper/product/ChangeToDtoProduct.java
+++ src/main/java/com/final_project/e_commerce/mapper/product/ChangeToDtoProduct.java
@@ -1,22 +1,38 @@
 package com.final_project.e_commerce.mapper.product;
 
+import com.final_project.e_commerce.data.domainData.responseDomainData.product.ResponseProductDomainData;
+import com.final_project.e_commerce.data.dto.responseDto.product.ResponseAllDtoProduct;
+import com.final_project.e_commerce.data.dto.responseDto.product.ResponseDtoProduct;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ChangeToDtoProduct {
-//    以下寫法係列明changeProductDomainToResponseDtoList會用changeDomainToDtoForList(listElementMapper)
+    //    以下寫法係列明changeProductDomainToResponseDtoList會用changeDomainToDtoForList(listElementMapper)
 //
 //    ResponseDtoProduct changeProductDomainToResponseDto(ResponseProductDomainData domainData);
 //
-//    @Named("listElementMapper")
-//    @Mapping(target = "stock", ignore = true)
-//    @Mapping(target = "hasStock", expression = "java(judgeHasStock(responseProductDomainData))")
-//    ResponseDtoProduct changeDomainToDtoForList(ResponseProductDomainData responseProductDomainData);
 //
-//    @IterableMapping(qualifiedByName = "listElementMapper")
-//    List<ResponseDtoProduct> changeProductDomainToResponseDtoList(List<ResponseProductDomainData> responseDomainData);
+//    @Mapping(target = "hasStock", expression = "java(judgeHasStock(responseProductDomainData))")
+//    ResponseAllDtoProduct changeDomainToDtoForList(ResponseProductDomainData responseProductDomainData);
+//
+//
+//    List<ResponseAllDtoProduct> changeProductDomainToResponseDtoList(List<ResponseProductDomainData> responseDomainData);
 //
 //    default boolean judgeHasStock(ResponseProductDomainData responseProductDomainData) {
 //        return responseProductDomainData != null && responseProductDomainData.getStock() != null && responseProductDomainData.getStock() > 0;
 //    }
+    ResponseAllDtoProduct changeDomainToDtoForList(ResponseProductDomainData responseProductDomainData);
+
+    default Boolean stockToHasStock(Integer stock) {
+        return stock != null && stock > 0;
+    }
+
+    List<ResponseAllDtoProduct> changeProductDomainToResponseDtoList(List<ResponseProductDomainData> responseProductDomainDataList);
+
+    ResponseDtoProduct changeProductDomainToResponseDto(ResponseProductDomainData responseProductDomainData);
 }
